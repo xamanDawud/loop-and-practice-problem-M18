6c64e0f4cc966d125660b3802a045e5c829e014d
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-06 09:52:11 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,10 @@
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+//   console.log("Damn it is working " + i);
+// }
+
+var numbers = [23, 24, 56, 32, 65];
+
+for (var i = 0; i < 5; i++) {
+  console.log(numbers[i]);
+}
---
+++ while.js
@@ -0,0 +1,13 @@
+var num = 0;
+var people = ["salam", "Kalam", "Jabbar", "Kaffar"];
+while (num < people.length) {
+  console.log(people[num]);
+  num++;
+}
+
+var numbers = 1;
+
+while (numbers < 100) {
+  console.log(numbers);
+  numbers = numbers + 2;
+}
